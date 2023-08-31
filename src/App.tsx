dbc604348806b60d5bb595ff1ece23ffe864b785
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2023-08-31 21:46:27 +0200

    new project and json db

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import AddGuineaPig from "./AddGuineaPig";
+import ShowGuineaPigs from "./ShowGuineaPigs";
+
+export interface Guineapig {
+  id: number;
+  name: string;
+  age: number;
+  likes?: string;
+}
+
+function App() {
+  const [guineapigs, setGuineapigs] = useState<Guineapig[]>([]);
+
+  const getGuineapigs = () => {
+    fetch("http://localhost:3000/guineapigs")
+      .then((res) => res.json())
+      .then((data: Guineapig[]) => {
+        setGuineapigs(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getGuineapigs();
+  }, []);
+
+  const addNewGuineaPig = (newPig: Guineapig) => {
+    fetch("http://localhost:3000/guineapigs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPig),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        getGuineapigs();
+      });
+  };
+
+  return (
+    <div style={{ display: "flex", gap: "3rem" }}>
+      <AddGuineaPig addNewGuineapig={addNewGuineaPig} />
+      <ShowGuineaPigs guineapigs={guineapigs} />
+    </div>
+  );
+}
+
+export default App;
